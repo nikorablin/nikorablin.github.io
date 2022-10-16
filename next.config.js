0e13693647cc62eb4c64aedb3159d3947959fd94
Nik Korablin <nikorablin@gmail.com>
2022-10-16 13:56:22 -0400

    feat: Add deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: isProd ? '/nikorablin.github.io/' : '',
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/i,
