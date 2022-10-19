d2e85500e710d71f3fc09d215a57d5b7c4dcd42a
Nik Korablin <nikorablin@gmail.com>
2022-10-19 17:11:00 -0400

    feat: Add semantic-release

---
+++ release.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  branches: ['development'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
