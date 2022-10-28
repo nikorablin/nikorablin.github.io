1db699725c6ee9ef5a9f16e243f5bf3f95b73472
Nik Korablin <nikorablin@gmail.com>
2022-10-28 16:18:17 -0400

    build: Update config to ignore pkg updates

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,4 +3,5 @@ module.exports = {
   rules: {
     'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
   },
+  ignores: [(message) => /^chore\(release\): set `package.json` to \d.\d.\d \[skip ci\]/m.test(message)]
 };
